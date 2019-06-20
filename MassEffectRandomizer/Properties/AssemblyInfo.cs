09579918c7c27af34372eda046fe790bd66db948
Mgamerz <mgamerz2@gmail.com>
2019-06-20 08:31:00 -0600

    Make MER intro skippable in kismet

--- MassEffectRandomizer/Properties/AssemblyInfo.cs
+++ MassEffectRandomizer/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("0.0.0.341")]
-[assembly: AssemblyFileVersion("0.0.0.341")]
+[assembly: AssemblyVersion("0.0.0.351")]
+[assembly: AssemblyFileVersion("0.0.0.351")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
