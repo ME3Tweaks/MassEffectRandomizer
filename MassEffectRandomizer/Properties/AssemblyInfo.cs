d68d1a9312254e6e10e8cd4b9a3875c91d891d02
Mgamerz <mgamerz2@gmail.com>
2019-07-20 21:36:33 -0600

    Update planet info xml, image credits

--- MassEffectRandomizer/Properties/AssemblyInfo.cs
+++ MassEffectRandomizer/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("0.0.0.509")]
-[assembly: AssemblyFileVersion("0.0.0.509")]
+[assembly: AssemblyVersion("0.0.0.514")]
+[assembly: AssemblyFileVersion("0.0.0.514")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
