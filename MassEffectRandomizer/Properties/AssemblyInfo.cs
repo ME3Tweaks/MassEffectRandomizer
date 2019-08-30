9b9a373014f419f66e33a462929d22a149c910a1
Mgamerz <mgamerz2@gmail.com>
2019-08-29 23:05:02 -0600

    Correct a link in the imagecredits

--- MassEffectRandomizer/Properties/AssemblyInfo.cs
+++ MassEffectRandomizer/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 
 // Version informationr(
-[assembly: AssemblyVersion("1.0.0.999")]
-[assembly: AssemblyFileVersion("1.0.0.999")]
+[assembly: AssemblyVersion("1.0.0.1000")]
+[assembly: AssemblyFileVersion("1.0.0.1000")]
 [assembly: NeutralResourcesLanguageAttribute( "en-US" )]
 
